d6e7fe5d91f7c7b239b2d4afb757b5165662c6e8
Ecem Kaynar <ekaynar18@outlook.com>
2024-12-18 15:27:06 +0200

    splash screen and create profile screen added, register and login screen fixed

--- App.tsx
+++ App.tsx
@@ -1,17 +1,21 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import SplashScreen from './src/screens/splash/SplashScreen';
 import LoginScreen from './src/screens/login/LoginScreen';
 import RegisterScreen from './src/screens/register/RegisterScreen';
+import CreateProfileScreen from './src/screens/profile/CreateProfileScreen';
 
 const Stack = createNativeStackNavigator();
 
 const App = () => {
     return (
         <NavigationContainer>
-            <Stack.Navigator initialRouteName="Login">
+            <Stack.Navigator initialRouteName="Splash" screenOptions={{ headerShown: false }}>
+                <Stack.Screen name="Splash" component={SplashScreen} />
                 <Stack.Screen name="Login" component={LoginScreen} />
                 <Stack.Screen name="Register" component={RegisterScreen} />
+                <Stack.Screen name="CreateProfile" component={CreateProfileScreen} />
             </Stack.Navigator>
         </NavigationContainer>
     );
---
+++ src/screens/splash/SplashScreen.js
@@ -0,0 +1,51 @@
+import React, {useEffect} from 'react';
+import {View, Text, StyleSheet, Image} from 'react-native';
+import LinearGradient from 'react-native-linear-gradient';
+
+const SplashScreen = ({navigation}) => {
+  // Automatically navigate to LoginScreen after 3 seconds
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      navigation.replace('Login'); // Navigate to LoginScreen
+    }, 3000); // 3 seconds delay
+
+    return () => clearTimeout(timer); // Clean up timer
+  }, [navigation]);
+
+  return (
+    <LinearGradient colors={['#A95CF1', '#DB6FDF']} style={styles.container}>
+      <Image
+        source={require('../../../assets/images/gym_icon.png')} // Add your app logo here
+        style={styles.logo}
+      />
+      <Text style={styles.appName}>GymTastic</Text>
+      <Text style={styles.tagline}>Everybody Can Train</Text>
+    </LinearGradient>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  logo: {
+    width: 150,
+    height: 150,
+    marginBottom: 20,
+  },
+  appName: {
+    fontSize: 32,
+    fontWeight: 'bold',
+    color: '#fff',
+    marginBottom: 10,
+  },
+  tagline: {
+    fontSize: 16,
+    color: '#fff',
+    fontStyle: 'italic',
+  },
+});
+
+export default SplashScreen;
